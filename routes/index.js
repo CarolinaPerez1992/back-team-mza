d8f87b47f8d9d563766280d52ecb4da65f444e0a
CarolinaPerez1992 <carolinabperez@outlook.com>
2022-11-11 23:47:12 -0300

    MT-01-20

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router()
 const user = require('./user')
 const city = require('./city')
+const itinerary = require('./itinerary')
 
 router.use('/api/users', user)
 router.use('/api/cities', city)
+router.use('/api/itineraries', itinerary)
 module.exports = router;
+
