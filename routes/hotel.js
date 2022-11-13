0c5e183a914882b39df3d6dd3b9c2254069e3a81
Raulmoyano2970 <raulmoyano2970@gmail.com>
2022-11-13 01:40:05 -0300

    MT-02-24

--- routes/hotel.js
+++ routes/hotel.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-const { create } = require("../controllers/hotel"); //sacar read y onlyone ante depos post
+const { create, read } = require("../controllers/hotel"); //sacar read y onlyone ante depos post
 
-router.post("/", create);
-
+router.post("/", create); //post usamos en la primera
+router.get("/", read); //post usamos en la segunda
 module.exports = router;
