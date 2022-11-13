76ad89bd03bedbac53d94345dfedc8daf1db0050
CarolinaPerez1992 <carolinabperez@outlook.com>
2022-11-13 15:46:44 -0300

    MT-B1-27

--- routes/city.js
+++ routes/city.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
-const {create, read, readOnlyOne} = require('../controllers/city')
+const {create, read, readOnlyOne, update} = require('../controllers/city')
 
 
 router.post('/', create)
 router.get('/', read)
 router.get('/:id', readOnlyOne)
+router.put('/:id', update)
 
 module.exports = router;
