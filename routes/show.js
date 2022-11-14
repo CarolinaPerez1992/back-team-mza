97d2d94929fc316938f0b5e688a3a2d5764654be
Raulmoyano2970 <raulmoyano2970@gmail.com>
2022-11-14 14:46:21 -0300

    MT-B2-34

--- routes/show.js
+++ routes/show.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
-const { create, readShows } = require("../controllers/show");
+const { create, readShows, update } = require("../controllers/show");
 
 router.post("/", create);
 router.get("/", readShows);
+router.patch("/:id", update);
 module.exports = router;
