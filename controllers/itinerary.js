f782e60da834a257404ea47bb31cdf9808406df5
CarolinaPerez1992 <carolinabperez@outlook.com>
2022-11-13 22:43:11 -0300

    MT-B1-33

--- controllers/itinerary.js
+++ controllers/itinerary.js
@@ -46,6 +46,28 @@ const controller = {
             })
         }
     },
+    update: async(req,res) => {
+        let { id } = req.params
+        try {
+            let itinerary = await Itinerary.findOneAndUpdate({ _id: id }, req.body,{ new: true })
+            if (itinerary) {
+                res.status(200).json({
+                    success: true,
+                    message: "The itinerary was successfully modified"
+                })
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: "There is no itinerary that matches"
+                })
+            }
+        } catch(error) {
+            res.status(400).json({
+                success: false,
+                message: error.message
+            })
+        }
+    },
 }
 
 module.exports = controller;
