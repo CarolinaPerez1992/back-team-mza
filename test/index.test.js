6bb15d6a8daa2995351bbbe007e45ee5bf135bbf
CarolinaPerez1992 <carolinabperez@outlook.com>
2022-11-19 09:57:20 -0300

    FIX-MT-01-37

--- test/index.test.js
+++ test/index.test.js
@@ -9,9 +9,11 @@ describe('GET /api/cities', function(){
     it('verify that it is an array of cities', function(done){
         request(app)
         .get('/api/cities/')
-        .expect(response => {
-            assert.typeOf(response.body.data, 'array', 'Is Array')
-        })
+        .expect(res => {
+            assert.isArray(res.body.data)
+            res.body.data.forEach(city => {
+                assert.isObject(city)
+        })})
         .end(function(err, res){
             if(err){
                 return done(err)
