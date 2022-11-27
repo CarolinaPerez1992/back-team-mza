acf228f52823dd259566363735e26443a400ddf5
Raulmoyano2970 <raulmoyano2970@gmail.com>
2022-11-27 01:55:51 -0300

    MT-01-49

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,21 @@
 const router = require('express').Router();
 const schema  = require('../schemas/user');
 const validator = require('../middlewares/validator')
-const {register, check, logIn, loginWithToken} = require('../controllers/user');
+const {register, check, logIn, signoff, loginWithToken} = require('../controllers/user');
 const {accountExists} = require ("../middlewares/accountExistsSignUp")
 const accountExistsSignIn = require ("../middlewares/accountExistsSignIn")
 const schemaSignIn = require('../schemas/signin')
 const {accountHasBeenVerified} = require('../middlewares/accountHasBeenVerified')
 const passport = require('../config/passport')
 const mustSignIn = require('../middlewares/mustSignIn')
-router.post('/signUp',validator(schema)  , accountExists, register)
-router.get("/verify/:code" , check)
 
-router.post('/signin', validator(schemaSignIn), accountExistsSignIn, accountHasBeenVerified, logIn)
+router.post('/sign-up',validator(schema)  , accountExists, register)
+router.post('/sign-in', validator(schemaSignIn), accountExistsSignIn, accountHasBeenVerified, logIn)
+router.post("/sign-out", passport.authenticate("jwt",{session:false}), signoff)
+
+
+
+router.get("/verify/:code" , check)
 router.post('/token', passport.authenticate('jwt', {session: false}), mustSignIn, loginWithToken )
 
 module.exports = router;
